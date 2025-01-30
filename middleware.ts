9f73969dfc0ee77f3d94849a2b3c0cd783c3642a
Kiran5655 <142571315+Kiran5655@users.noreply.github.com>
2025-01-30 13:57:15 +0530

    again

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,19 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
-
+ 
 export default clerkMiddleware();
-
+ 
+// Stop Middleware running on static files and public routes
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next
+     * - static (static files)
+     * - favicon.ico (favicon file)
+     * - public folder
+     * - public files with extensions (.html, .css, .js, etc)
+     */
+    "/((?!_next|static|.*\\..*|favicon.ico).*)",
+    "/(api|trpc)(.*)",
   ],
 };
