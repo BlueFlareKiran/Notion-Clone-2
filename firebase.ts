f8374640097f53613151be99c0145748803df6b6
Kiran5655 <142571315+Kiran5655@users.noreply.github.com>
2025-01-30 11:20:28 +0530

    test 2

--- firebase.ts
+++ firebase.ts
@@ -3,8 +3,9 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyDFjLTpishgxudGPtalbUTO9Kw9vYRJNo4",
   authDomain: "notion-clone-259c1.firebaseapp.com",
-  projectId: "notion-clone-259c1", // Must match your project ID
+  projectId: "notion-clone-259c1",
   storageBucket: "notion-clone-259c1.firebasestorage.app",
+  messagingSenderId: "895469567785",
   appId: "1:895469567785:web:1fe7ec001046ea6180c138"
 };
   const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
